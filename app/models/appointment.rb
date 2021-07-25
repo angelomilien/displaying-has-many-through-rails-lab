d6d9afb8a8df55bb4c37aaa3c0b0d594644e1985
Angelo <angelomilienjoseph@gmail.com>
2021-07-25 18:15:52 -0400

    commit tree c467af95a62c757485d84052ffab5d27cbba4ba7

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,11 +4,11 @@ class Appointment < ApplicationRecord
   
     def doctor_name=(name)
        self.doctor = Doctor.find_or_create_by(name: name)
-     end
+    end
   
-     def doctor_name
-        self.doctor ? self.doctor.name : nil
-     end
+    def doctor_name
+       self.doctor ? self.doctor.name : nil
+    end
   
     def patient_name=(name)
       self.patient = Patient.find_or_create_by(name: name)
